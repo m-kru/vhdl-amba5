e6063851ed35d7998ab5f960e27d03c57463f5e6
Michał Kruszewski <mkru@protonmail.com>
2024-02-27 15:47:33 +0100

    apb: bfm: Add readb procedure

--- apb/bfm.vhd
+++ apb/bfm.vhd
@@ -45,7 +45,7 @@ package bfm is
   );
 
   -- The read procedure carries out read transaction with a single read transfer.
-  -- There is no data out parameter as the is available in the iface.rdata attribute.
+  -- There is no data out parameter as the data is available in the iface.rdata element.
   procedure read (
     constant addr  : in unsigned(31 downto 0);
     signal   clk   : in std_logic;
@@ -70,7 +70,22 @@ package bfm is
     constant auser : in std_logic_vector(127 downto 0) := (others => '-');
     constant wuser : in std_logic_vector( 15 downto 0) := (others => '-');
     constant slverr_severity : in severity_level := error; -- Severity of the report message when slverr is asserted by the Completer.
-    constant exit_on_slverr  : in boolean := true;
+    constant exit_on_slverr  : in boolean := true; -- Exit procedure when Completer asserts slverr.
+    constant cfg   : in config_t := DEFAULT_CONFIG;
+    constant msg   : in string := "" -- An optional user message added at the end of the report message.
+  );
+
+  -- The readb procedure carries out block read transaction with multiple read transfers.
+  procedure readb (
+    constant addr  : in unsigned(31 downto 0); -- Start address
+    signal   data  : out data_array_t;
+    signal   clk   : in std_logic;
+    signal   iface : view requester_view;
+    constant prot  : in protection_t := (data_instruction => '0', secure_non_secure => '0', normal_privileged => '0');
+    constant nse   : in std_logic := '-';
+    constant auser : in std_logic_vector(127 downto 0) := (others => '-');
+    constant slverr_severity : in severity_level := error; -- Severity of the report message when slverr is asserted by the Completer.
+    constant exit_on_slverr  : in boolean := true; -- Exit procedure when Completer asserts slverr.
     constant cfg   : in config_t := DEFAULT_CONFIG;
     constant msg   : in string := "" -- An optional user message added at the end of the report message.
   );
@@ -335,5 +350,106 @@ package body bfm is
     wait for 0 ns;
     wait for 0 ns;
   end procedure writeb;
+
+  procedure readb (
+    constant addr  : in unsigned(31 downto 0);
+    signal   data  : out data_array_t;
+    signal   clk   : in std_logic;
+    signal   iface : view requester_view;
+    constant prot  : in protection_t := (data_instruction => '0', secure_non_secure => '0', normal_privileged => '0');
+    constant nse   : in std_logic := '-';
+    constant auser : in std_logic_vector(127 downto 0) := (others => '-');
+    constant slverr_severity : in severity_level := error;
+    constant exit_on_slverr  : in boolean := true;
+    constant cfg   : in config_t := DEFAULT_CONFIG;
+    constant msg   : in string := ""
+  ) is
+    constant initial_wakeup : std_logic := iface.wakeup;
+  begin
+    report cfg.prefix & "readb: addr => x""" & to_hstring(addr) & """, data length => " & to_string(data'length) & msg;
+
+    iface.addr  <= addr;
+    iface.prot  <= prot;
+    iface.nse   <= nse;
+    iface.write <= '0';
+    iface.strb  <= (others => '0');
+    iface.auser <= auser;
+    wait for 0 ns;
+    wait for 0 ns;
+
+    -- Assert wakeup signal if it is not yet asserted
+    if initial_wakeup /= '1' then
+      iface.wakeup <= '1';
+      wait until rising_edge(clk) for cfg.timeout;
+      if clk /= '1' then
+        report cfg.prefix & "timeout while waiting for clk to assert wakeup" severity cfg.timeout_severity;
+      end if;
+    end if;
+
+    -- Enter SETUP state
+    iface.selx <= '1';
+    iface.enable <= '0';
+    wait until rising_edge(clk) for cfg.timeout;
+    if clk /= '1' then
+      report cfg.prefix & "timeout while waiting for clk to enter SETUP state" severity cfg.timeout_severity;
+    end if;
+
+    -- Enter ACCESS state
+    iface.enable <= '1';
+    wait until rising_edge(clk) for cfg.timeout;
+    if clk /= '1' then
+      report cfg.prefix & "timeout while entering ACCESS state" severity cfg.timeout_severity;
+    end if;
+
+    -- Data read loop
+    for i in data'left to data'right loop
+      -- Wait until ready
+      if iface.ready /= '1' then
+        wait until rising_edge(iface.ready) for cfg.timeout;
+        if iface.ready /= '1' then
+          report cfg.prefix & "timeout while waiting for Completer to assert ready" severity cfg.timeout_severity;
+        end if;
+      end if;
+
+      -- Report error if asserted
+      if iface.slverr = '1' then
+        report cfg.prefix & "Completer indicates error" severity slverr_severity;
+        if exit_on_slverr then exit; end if;
+      end if;
+
+      data(i) <= iface.rdata;
+
+      if i = data'right then
+        exit;
+      end if;
+      
+      -- Reenter SETUP state
+      iface.addr <= iface.addr + 1;
+      iface.enable <= '0';
+      wait until rising_edge(clk) for cfg.timeout;
+      if clk /= '1' then
+        report cfg.prefix & "data(" &  to_string(i+1) & "): " & "timeout while reentering SETUP state" severity cfg.timeout_severity;
+      end if;
+
+      -- Reenter ACCESS state
+      iface.enable <= '1';
+      wait until rising_edge(clk) for cfg.timeout;
+      if clk /= '1' then
+        report cfg.prefix & "data(" &  to_string(i+1) & "): " & "timeout while reentering ACCESS state" severity cfg.timeout_severity;
+      end if;
+    end loop;
+
+    -- Cleanup
+    iface.selx <= '0';
+    iface.enable <= '0';
+
+    -- Restore initial wakeup signal value
+    if initial_wakeup /= '1' then
+      iface.wakeup <= initial_wakeup;
+    end if;
+
+    wait for 0 ns;
+    wait for 0 ns;
+  end procedure readb;
 
 end package body;
---
+++ apb/tb/bfm/tb-readb.vhd
@@ -0,0 +1,77 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+library apb;
+  use apb.apb.all;
+  use apb.bfm;
+  use apb.checker.all;
+
+
+entity tb_readb is
+end entity;
+
+
+architecture test of tb_readb is
+
+  signal clk : std_logic := '1';
+
+  signal ck : checker_t := init;
+  signal iface : interface_t := init;
+
+  constant ADDR : unsigned(31 downto 0) := x"00000000";
+  constant DATA : data_array_t := (
+    x"01234567", x"89ABCDEF", x"DAEDBEEF", x"F0F0F0F0"
+  );
+
+  signal read_data : data_array_t(0 to 3);
+
+  signal write_done : boolean := false;
+
+begin
+
+  clk <= not clk after 0.5 ns;
+
+
+  interface_checker : process (clk) is
+  begin
+    if rising_edge(clk) then
+      ck <= clock(ck, iface);
+    end if;
+  end process;
+
+
+  mock_completer : process (clk) is
+  begin
+    if rising_edge(clk) then
+      iface.ready <= '1';
+      if iface.selx = '1'  then
+        iface.rdata <= DATA(to_integer(iface.addr));
+      end if;
+    end if;
+  end process;
+
+
+  main : process is
+  begin
+    wait for 2 ns;
+    bfm.readb(ADDR, read_data, clk, iface, msg => ", user msg");
+    write_done <= true;
+    wait;
+  end process;
+
+
+  data_checker : process is
+  begin
+    wait until write_done = true;
+
+    for i in read_data'range loop
+      assert read_data(i) = DATA(i)
+        report to_string(i) & ": got: " & to_hstring(read_data(i)) & ", want: " & to_hstring(DATA(i));
+    end loop;
+
+    wait for 3 ns;
+    std.env.finish;
+  end process;
+
+end architecture;
