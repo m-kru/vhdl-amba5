f67972d62c710403635fe20aa33d71850d03e300
Michał Kruszewski <mkru@protonmail.com>
2025-01-19 13:53:45 +0100

    Add some crossbar code

--- apb/crossbar.vhd
+++ apb/crossbar.vhd
@@ -12,17 +12,21 @@ library work;
 
 entity Crossbar is
   generic (
-    REPORT_PREFIX   : string := "apb: crossbar: " -- Prefix used in report messages
+    REPORT_PREFIX   : string := "apb: crossbar: "; -- Prefix used in report messages
     REQUESTER_COUNT : positive := 1;
     COMPLETER_COUNT : positive := 1;
     ADDRS : addr_array_t(0 to COMPLETER_COUNT - 1); -- Completer addresses
-    MASKS : mask_array_t(0 to COMPLETER_COUNT - 1); -- Completer address masks
+    MASKS : mask_array_t(0 to COMPLETER_COUNT - 1)  -- Completer address masks
   );
   port (
     arstn_i : in std_logic := '1';
     clk_i   : in std_logic;
-    requesters : view (completer_view) of interface_array_t(0 to REQUESTER_COUNT - 1); -- Connect requesters to this port
-    completers : view (requester_view) of interface_array_t(0 to COMPLETER_COUNT - 1)  -- Connect completers to this port
+    -- Requesters connections - crossbar is a completer
+    reqs_i : in  requester_out_array_t(0 to REQUESTER_COUNT - 1);
+    reqs_o : out requester_in_array_t (0 to REQUESTER_COUNT - 1);
+    -- Completers connections - crossbar is a requester
+    coms_i : in  completer_out_array_t(0 to COMPLETER_COUNT - 1);
+    coms_o : out completer_in_array_t (0 to COMPLETER_COUNT - 1)
   );
 end entity;
 
@@ -41,10 +45,10 @@ architecture rtl of Crossbar is
   -- addresses Completer with index 1.
   signal addr_matrix : matrix_t;
 
-  -- The selx_matrix contains information which Requester want to access a given Completer.
+  -- The selx_matrix contains information which Requester wants to access a given Completer.
   signal selx_matrix : matrix_t;
 
-  -- The conn_matrix is the connection matrix.
+  -- The conn_matrix is the current connection matrix.
   signal conn_matrix : matrix_t;
 
   -- Sanity checks
@@ -72,8 +76,8 @@ begin
     for c in completer_range loop
       for r in requester_range loop
         addr_matrix(c)(r) <= '0';
-        report to_debug(requesters(r));
-        if (requesters(r).addr and unsigned(to_std_logic_vector(MASKS(c)))) = ADDRS(c) then
+        --report to_debug(reqs_i(r));
+        if (reqs_i(r).addr and unsigned(to_std_logic_vector(MASKS(c)))) = ADDRS(c) then
           addr_matrix(c)(r) <= '1';
         end if;
       end loop;
@@ -86,7 +90,7 @@ begin
     for c in completer_range loop
       for r in requester_range loop
         selx_matrix(c)(r) <= '0';
-        if addr_matrix(c)(r) = '1' and requesters(r).selx = '1' then
+        if addr_matrix(c)(r) = '1' and reqs_i(r).selx = '1' then
           selx_matrix(c)(r) <= '1';
         end if;
       end loop;
@@ -131,51 +135,52 @@ begin
             severity failure;
         end if;
 
-        completers(c).addr   <= (others => '-');
-        completers(c).prot   <= ('-', '-', '-');
-        completers(c).nse    <= '-';
-        completers(c).selx   <= '0';
-        completers(c).enable <= '-';
-        completers(c).write  <= '-';
-        completers(c).wdata  <= (others => '-');
-        completers(c).strb   <= (others => '-');
-        completers(c).auser  <= (others => '-');
-        completers(c).wuser  <= (others => '-');
+        -- Deselect all completers by default
+        coms_o(c).addr   <= (others => '-');
+        coms_o(c).prot   <= ('-', '-', '-');
+        coms_o(c).nse    <= '-';
+        coms_o(c).selx   <= '0';
+        coms_o(c).enable <= '-';
+        coms_o(c).write  <= '-';
+        coms_o(c).wdata  <= (others => '-');
+        coms_o(c).strb   <= (others => '-');
+        coms_o(c).auser  <= (others => '-');
+        coms_o(c).wuser  <= (others => '-');
 
         if conn_matrix(c) = REQ_ZERO then
           for r in requester_range loop
             if selx_matrix(c)(r) = '1' then
               conn_matrix(c)(r) <= '1';
-              -- Generate SETUP entry condition for the Completer
-              completers(c).selx <= '1';
-              completers(c).enable <= '0';
+              -- Recreate SETUP entry condition for the Completer.
+              -- The requester might already asserted enable.
+              coms_o(c) <= reqs_i(r);
+              coms_o(c).enable <= '0';
               exit;
             end if;
           end loop;
-        -- There is already a connection between the Completer c and some Requester
-        else
+        else -- There is already a connection between the Completer c and some Requester
           r := hot_bit_idx(conn_matrix(c));
 
           -- End of connection
-          if requesters(r).selx = '0' then
-            conn_matrix(c) <= (others => '0'); -- TODO: Is it better to clear whole vector or single bit?
-          else -- Route interfaces
-            completers(c).addr   <= requesters(r).addr;
-            completers(c).prot   <= requesters(r).prot;
-            completers(c).nse    <= requesters(r).nse;
-            completers(c).selx   <= requesters(r).selx;
-            completers(c).enable <= requesters(r).enable;
-            completers(c).write  <= requesters(r).write;
-            completers(c).wdata  <= requesters(r).wdata;
-            completers(c).strb   <= requesters(r).strb;
-            completers(c).auser  <= requesters(r).auser;
-            completers(c).wuser  <= requesters(r).wuser;
+          if reqs_i(r).selx = '0' then
+            conn_matrix(c) <= (others => '0'); -- TODO: Is it better to clear whole vector or the single bit?
+          else -- Route interface signals
+            coms_o(c).addr   <= reqs_i(r).addr;
+            coms_o(c).prot   <= reqs_i(r).prot;
+            coms_o(c).nse    <= reqs_i(r).nse;
+            coms_o(c).selx   <= reqs_i(r).selx;
+            coms_o(c).enable <= reqs_i(r).enable;
+            coms_o(c).write  <= reqs_i(r).write;
+            coms_o(c).wdata  <= reqs_i(r).wdata;
+            coms_o(c).strb   <= reqs_i(r).strb;
+            coms_o(c).auser  <= reqs_i(r).auser;
+            coms_o(c).wuser  <= reqs_i(r).wuser;
 
-            requesters(r).ready  <= completers(c).ready;
-            requesters(r).rdata  <= completers(c).rdata;
-            requesters(r).slverr <= completers(c).slverr;
-            requesters(r).ruser  <= completers(c).ruser;
-            requesters(r).buser  <= completers(c).buser;
+            reqs_o(r).ready  <= coms_i(c).ready;
+            reqs_o(r).rdata  <= coms_i(c).rdata;
+            reqs_o(r).slverr <= coms_i(c).slverr;
+            reqs_o(r).ruser  <= coms_i(c).ruser;
+            reqs_o(r).buser  <= coms_i(c).buser;
           end if;
         end if;
       end loop;
--- apb/tb/crossbar/tb-two-requesters-one-completer.vhd
+++ apb/tb/crossbar/tb-two-requesters-one-completer.vhd
@@ -19,9 +19,17 @@ architecture test of tb_two_requesters_one_completer is
   signal bfm0_cfg : bfm.config_t := bfm.init(REPORT_PREFIX => "apb: bfm0: ");
   signal bfm1_cfg : bfm.config_t := bfm.init(REPORT_PREFIX => "apb: bfm1: ");
 
-  signal req0_iface, req1_iface, com_iface : interface_t := init;
+  -- Requesters interfaces
+  signal req0_out, req1_out : requester_out_t := init;
+  signal req0_in,  req1_in  : requester_in_t  := init;
 
-  signal req0_ck, req1_ck, com_ck : checker_t := init;
+  -- Completer interface
+  signal com_in  : completer_in_t  := init;
+  signal com_out : completer_out_t := init;
+
+  signal req0_ck : checker_t := init(REPORT_PREFIX => "apb: checker: req0: ");
+  signal req1_ck : checker_t := init(REPORT_PREFIX => "apb: checker: req1: ");
+  signal com_ck  : checker_t := init(REPORT_PREFIX => "apb: checker: com: ");
 
   signal mc : mock_completer_t := init(memory_size => 8);
 
@@ -47,9 +55,9 @@ begin
   interface_checkers : process (clk) is
   begin
     if rising_edge(clk) then
-      req0_ck <= clock(req0_ck, req0_iface);
-      req1_ck <= clock(req1_ck, req1_iface);
-      com_ck  <= clock(com_ck, com_iface);
+      req0_ck <= clock(req0_ck, req0_out, req0_in);
+      req1_ck <= clock(req1_ck, req1_out, req1_in);
+      com_ck  <= clock(com_ck,  com_in,   com_out);
     end if;
   end process;
 
@@ -57,7 +65,7 @@ begin
   requester_0 : process is
   begin
     wait until arstn = '1';
-    bfm.write(x"00000000", x"DEADBEEF", clk, req0_iface, cfg => bfm0_cfg);
+    bfm.write(x"00000000", x"DEADBEEF", clk, req0_out, req0_in, cfg => bfm0_cfg);
     wait for 2 ns;
     wait;
   end process;
@@ -66,7 +74,7 @@ begin
   requester_1 : process is
   begin
     wait until arstn = '1';
-    bfm.write(x"00000001", x"BEEFDEAD", clk, req1_iface, cfg => bfm1_cfg);
+    bfm.write(x"00000001", x"BEEFDEAD", clk, req1_out, req1_in, cfg => bfm1_cfg);
     wait for 2 ns;
     wait;
   end process;
@@ -75,7 +83,7 @@ begin
   completer : process (clk) is
   begin
     if rising_edge(clk) then
-      clock(mc, com_iface);
+      clock(mc, com_in, com_out);
     end if;
   end process;
 
@@ -88,9 +96,12 @@ begin
   ) port map (
     arstn_i => arstn,
     clk_i   => clk,
-    requesters(0) => req0_iface,
-    requesters(1) => req1_iface,
-    completers(0) => com_iface
+    reqs_i(0) => req0_out,
+    reqs_i(1) => req1_out,
+    reqs_o(0) => req0_in,
+    reqs_o(1) => req1_in,
+    coms_i(0) => com_out,
+    coms_o(0) => com_in
   );
 
 end architecture;
