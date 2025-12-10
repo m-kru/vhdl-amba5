bbf0d62368f00dc3bb0389161e2e3c36e7b3c846
Michał Kruszewski <mkru@protonmail.com>
2025-12-10 17:27:33 +0100

    crossbar: Add read transactions to testbench

--- apb/tb/crossbar/tb-three-reqs-one-com.vhd
+++ apb/tb/crossbar/tb-three-reqs-one-com.vhd
@@ -17,6 +17,8 @@ architecture test of tb_three_reqs_one_com is
   constant REQ_COUNT : natural := 3;
   subtype req_range is natural range 0 to REQ_COUNT - 1;
 
+  constant STAGE_TIMEOUT : time := 100 ns;
+
   signal arstn : std_logic := '0';
   signal clk : std_logic := '1';
 
@@ -34,14 +36,18 @@ architecture test of tb_three_reqs_one_com is
   signal com_in  : completer_in_t  := init;
   signal com_out : completer_out_t := init;
 
+  -- Requesters checkers
   signal req_cks : checker_array_t := (
     init(REPORT_PREFIX => "apb: checker: req 0: "),
     init(REPORT_PREFIX => "apb: checker: req 1: "),
     init(REPORT_PREFIX => "apb: checker: req 2: ")
   );
+
+  -- Completer checker
   signal com_ck : checker_t := init(REPORT_PREFIX => "apb: checker: com: ");
 
   signal req_write_done : boolean_vector(req_range) := (others => false);
+  signal req_read_done  : boolean_vector(req_range) := (others => false);
 
   signal mc : mock_completer_t := init(memory_size => 12);
 
@@ -56,6 +62,10 @@ architecture test of tb_three_reqs_one_com is
     1 => (x"66666666", x"77777777", x"88888888", x"99999999"),
     2 => (x"BBBBBBBB", x"CCCCCCCC", x"DDDDDDDD", x"EEEEEEEE")
   );
+
+  signal READ_DATA : data_vector_2d_t(req_range)(0 to 3);
+
+  signal write_checker_done, read_checker_done : boolean := false;
 
 begin
 
@@ -92,11 +102,27 @@ requesters : for r in req_range generate
   requester_0 : process is
   begin
     wait until arstn = '1';
+
+    -- Write test
     for i in WRITE_DATA(r)'range loop
       bfm.write(ADDRS(r) + to_unsigned(i * 4, 32), WRITE_DATA(r)(i), clk, req_outs(r), req_ins(r), cfg => bfm_cfgs(r));
       wait for 2 ns;
     end loop;
     req_write_done(r) <= true;
+
+    wait until req_write_done = (true, true, true);
+
+    -- Read test
+    -- Each requester reads data written by the next requester.
+    for i in WRITE_DATA(r)'range loop
+      bfm.read(ADDRS((r+1) mod REQ_COUNT) + to_unsigned(i * 4, 32), clk, req_outs(r), req_ins(r), cfg => bfm_cfgs(r));
+      READ_DATA(r)(i) <= req_ins(r).rdata;
+      wait for 2 ns;
+    end loop;
+    req_read_done(r) <= true;
+
+    wait until req_read_done = (true, true, true);
+
     wait;
   end process;
 end generate;
@@ -143,24 +169,89 @@ end generate;
   begin
     if rising_edge(clk) then
       assert req_write_done(0) = true or req_write_done(1) = false
-        report "requester 1 finished before requester 0";
+        report "requester 1 finished write before requester 0";
       assert req_write_done(0) = true or req_write_done(2) = false
-        report "requester 2 finished before requester 0";
+        report "requester 2 finished write before requester 0";
       assert req_write_done(1) = true or req_write_done(2) = false
-        report "requester 2 finished before requester 1";
+        report "requester 2 finished write before requester 1";
     end if;
+  end process;
+
+
+  read_order_checker : process (clk) is
+  begin
+    if rising_edge(clk) then
+      assert req_read_done(0) = true or req_read_done(1) = false
+        report "requester 1 finished read before requester 0";
+      assert req_read_done(0) = true or req_read_done(2) = false
+        report "requester 2 finished read before requester 0";
+      assert req_read_done(1) = true or req_read_done(2) = false
+        report "requester 2 finished read before requester 1";
+    end if;
+  end process;
+
+
+  write_checker : process is
+    variable got, want : std_logic_vector(31 downto 0);
+  begin
+    -- Wait for writes to finish.
+    wait for STAGE_TIMEOUT;
+
+    -- Write final asserts
+    assert req_write_done = (true, true, true)
+      report "not all requesters finished write transactions, req_write_done = " & to_string(req_write_done);
+    assert mc.write_count = 12
+      report "invalid write count, got: " & to_string(mc.write_count) & ", want: 12";
+    -- Check written data
+    for r in req_range loop
+      for d in WRITE_DATA(r)'range loop
+        got  := mc.memory(r*4+d);
+        want := WRITE_DATA(r)(d);
+        assert got = want
+          report "requester " & to_string(r) & ": invalid write data " & to_string(d) &
+            ": got 0x" & to_hstring(got) & ", want: 0x" & to_hstring(want);
+      end loop;
+    end loop;
+
+    write_checker_done <= true;
+    wait;
+  end process;
+
+
+  read_checker : process is
+    variable got, want : std_logic_vector(31 downto 0);
+  begin
+    -- Wait for reads to finish.
+    wait for 2 * STAGE_TIMEOUT;
+
+    -- Read final asserts
+    assert req_read_done = (true, true, true)
+      report "not all requesters finished read transactions, req_read_done = " & to_string(req_read_done);
+    assert mc.read_count = 12
+      report "invalid read count, got: " & to_string(mc.write_count) & ", want: 12";
+
+    -- Check read data
+    for r in req_range loop
+      for d in READ_DATA(r)'range loop
+        got  := READ_DATA(r)(d);
+        want := mc.memory(((r+1) mod REQ_COUNT)*4 + d);
+        assert got = want
+          report "requester " & to_string(r) & ": invalid read data " & to_string(d) &
+            ": got 0x" & to_hstring(got) & ", want: 0x" & to_hstring(want);
+      end loop;
+    end loop;
+
+    read_checker_done <= true;
+    wait;
   end process;
 
 
   main : process is
   begin
-    wait for 100 ns;
+    wait for 4 * STAGE_TIMEOUT;
 
-    -- Some final asserts
-    assert req_write_done = (true, true, true)
-      report "not all requesters finished write transactions, req_write_done = " & to_string(req_write_done);
-    assert mc.write_count = 12
-      report "invalid write count, got: " & to_string(mc.write_count) & ", want: 12";
+    assert write_checker_done report "write checker hasn't finished";
+    assert read_checker_done  report "read checker hasn't finished";
 
     std.env.finish;
   end process;
